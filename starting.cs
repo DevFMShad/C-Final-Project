771d898617e992fef4bdead175fbfd0dfccd8f1a
Fuad Mahmud Shad <fuademrite@gmail.com>
2025-05-27 22:47:11 +0300

    Starting out
    Nothing to say so far

---
+++ starting.cs
@@ -0,0 +1,7 @@
+// well this is just a start 
+// from here on out I will talk about the propject and what I have learned about the project so far
+// my understanding of things and I will keeep on commiting them in github
+// Now I understand that it a bit tough project but I have had prior knowledge of C# before
+// so it could be easy for me or the basic could be a bit tough
+// does not matter either way, casuse I will do it and not matter what
+// I will do it better than most
